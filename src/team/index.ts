0ab9ccff8da86647f5a8c129001054c6ef35abcd
Zain Babar <zainbabar@Zains-MacBook-Pro.local>
2025-02-06 21:08:04 -0500

    Add Zain to team

--- src/team/index.ts
+++ src/team/index.ts
@@ -1,10 +1,12 @@
 import { TeamMember } from "../types/TeamMember";
 import { exampleMember } from "./example";
 import { amir } from "./amir"; 
+import { Zain } from "./ZainBabarr"; 
 
 // Import your file here and add it to the array!
 export const teamMembers: TeamMember[] = [
     amir,
+    Zain,
     exampleMember,
     // Add yourself here!
 ]; 
