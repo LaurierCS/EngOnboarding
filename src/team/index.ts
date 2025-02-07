b8cc32b13856f9e74360dcd3f2e9084ddc707669
Sharib Kabani <ksharib05@gmail.com>
2025-02-06 20:33:04 -0500

    Adding sharib to cards

--- src/team/index.ts
+++ src/team/index.ts
@@ -1,10 +1,12 @@
 import { TeamMember } from "../types/TeamMember";
 import { exampleMember } from "./example";
 import { amir } from "./amir"; 
+import { sharib } from "./sharib";
 
 // Import your file here and add it to the array!
 export const teamMembers: TeamMember[] = [
     amir,
+    sharib,
     exampleMember,
     // Add yourself here!
 ]; 
