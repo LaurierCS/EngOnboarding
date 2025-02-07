d9ae297c720c5a6ba96cec1618abbe847f54e698
Markus-Fung <markusfung04@gmail.com>
2025-02-06 21:12:10 -0500

    Added Card

--- src/team/index.ts
+++ src/team/index.ts
@@ -1,10 +1,12 @@
 import { TeamMember } from "../types/TeamMember";
 import { exampleMember } from "./example";
 import { amir } from "./amir"; 
+import { markus } from "./markus";
 
 // Import your file here and add it to the array!
 export const teamMembers: TeamMember[] = [
     amir,
     exampleMember,
+    markus,
     // Add yourself here!
 ]; 
