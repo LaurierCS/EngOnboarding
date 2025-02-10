ba5449cc3598e979d89f58c66eed8188686f7cab
Abdullah Wasiq <wasiq.abdullah.21@gmail.com>
2025-02-09 20:54:37 -0500

    Add Abdullah to team

---
+++ src/team/abdullah.ts
@@ -0,0 +1,14 @@
+import { TeamMember } from "../types/TeamMember";
+
+export const abdullah: TeamMember = {
+    name: "Abdullah Wasiq",
+    role: "Software Engineer",
+    funFact: "I'm bored",
+    githubUsername: "aftwasiq",
+    // Feel free to customize your card's styles!
+    cardStyle: {
+        background: "bg-gradient-to-br from-purple-900 to-purple-700",
+        border: "border border-slate-700",
+        text: "text-slate-300"
+    }
+}; 
--- src/team/index.ts
+++ src/team/index.ts
@@ -7,6 +7,7 @@ import { Zain } from "./ZainBabarr";
 import { Jeffrey } from "./Jeffrey";
 import { david } from "./david";
 import { michael } from "./michael";
+import { abdullah } from "./abdullah";
 
 
 // Import your file here and add it to the array!
@@ -18,6 +19,7 @@ export const teamMembers: TeamMember[] = [
     markus,
     Jeffrey,
     david,
-    michael
+    michael,
+    abdullah
     // Add yourself here!
 ]; 
