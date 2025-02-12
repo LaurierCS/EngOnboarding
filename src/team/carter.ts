7540f95714c9defb32db51f566a7c35a537c63b3
carterchan9 <carterchan27@gmail.com>
2025-02-12 18:17:32 -0500

    Add Carter

---
+++ src/team/carter.ts
@@ -0,0 +1,14 @@
+import { TeamMember } from "../types/TeamMember";
+
+export const carter: TeamMember = {
+    name: "Carter Chan",
+    role: "Software Engineer/PM",
+    funFact: "I love chicken wings!",
+    githubUsername: "carterchan9",
+    // Feel free to customize your card's styles!
+    cardStyle: {
+        background: "bg-slate-800",
+        border: "border border-slate-700",
+        text: "text-slate-300"
+    }
+}; 
--- src/team/index.ts
+++ src/team/index.ts
@@ -10,6 +10,7 @@ import { Jeffrey } from "./Jeffrey";
 import { michael } from "./michael";
 import { abdullah } from "./abdullah";
 import { kian } from "./kian";
+import { carter } from "./carter";
 
 
 // Import your file here and add it to the array!
@@ -25,6 +26,7 @@ export const teamMembers: TeamMember[] = [
     abdullah,
     kian,
     Zahra,
-    michael
+    michael,
+    carter
     // Add yourself here!
 ]; 
