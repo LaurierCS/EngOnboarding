3c3d08e61e04e421adc5b68859fae737167798af
Xavier D'Mello <xaviermdmello@gmail.com>
2025-02-13 14:01:59 -0800

    Add Xavier to team

---
+++ src/team/xavier.ts
@@ -0,0 +1,14 @@
+import { TeamMember } from "../types/TeamMember";
+
+export const exampleMember: TeamMember = {
+    name: "Xavier D'Mell",
+    role: "Software Engineer",
+    funFact: "I like dogs!",
+    githubUsername: "xavierdmello",
+    // Feel free to customize your card's styles!
+    cardStyle: {
+        background: "bg-red-800",
+        border: "border border-red-700",
+        text: "text-red-300"
+    }
+}; 
