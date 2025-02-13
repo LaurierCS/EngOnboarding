a671f6f631eadaa2b6ba63e8614fe0e4e378180d
jariger <jariger2004@gmail.com>
2025-02-12 19:38:35 -0500

    Added my card :D

---
+++ src/team/YariG.ts
@@ -0,0 +1,14 @@
+import { TeamMember } from "../types/TeamMember";
+
+export const jari: TeamMember = {
+    name: "Jari Gerritsen",
+    role: "Spectator",
+    funFact: "I fancy rock climbing!",
+    githubUsername: "jariger",
+    // Feel free to customize your card's styles!
+    cardStyle: {
+        background: "bg-slate-800",
+        border: "border border-slate-700",
+        text: "text-slate-300"
+    }
+};
