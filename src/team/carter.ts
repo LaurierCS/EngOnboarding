5503e347e0e51292593af4b8443e2d54ddae37ec
carterchan9 <carterchan27@gmail.com>
2025-09-25 20:19:53 -0400

    Add carter to team

---
+++ src/team/carter.ts
@@ -0,0 +1,14 @@
+import { TeamMember } from "../types/TeamMember";
+
+export const sharib: TeamMember = {
+  name: "Carter Chan",
+  role: "VP, Engineering",
+  funFact: "I love chicken wings!",
+  githubUsername: "carterchan9",
+  // Feel free to customize your card's styles!
+  cardStyle: {
+    background: "bg-purple-000",
+    border: "border border-slate-300",
+    text: "text-slate-300",
+  },
+};
