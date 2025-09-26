8a248e95549327866917d8035e757524f25a67a3
sharibkabani <ksharib05@gmail.com>
2025-09-25 20:05:35 -0400

    Welcome to Eng 2025/26!

--- src/team/index.ts
+++ src/team/index.ts
@@ -1,37 +1,4 @@
 import { TeamMember } from "../types/TeamMember";
-import { exampleMember } from "./example";
-import { amir } from "./amir"; 
-import { markus } from "./markus";
 import { sharib } from "./sharib";
-import { Zain } from "./ZainBabarr"; 
-import { david } from "./david"
-import { Zahra } from "./Zahra"
-import { Jeffrey } from "./Jeffrey";
-import { michael } from "./michael";
-import { abdullah } from "./abdullah";
-import { kian } from "./kian";
-import { carter } from "./carter";
-import { YariG } from "./YariG";
-import { xavier } from "./xavier";
-import { Bradley } from "./Bradley";
 
-// Import your file here and add it to the array!
-export const teamMembers: TeamMember[] = [
-    amir,
-    sharib,
-    Zain,
-    exampleMember,
-    markus,
-    Jeffrey,
-    david,
-    michael,
-    abdullah,
-    kian,
-    Zahra,
-    michael,
-    carter,
-    YariG,
-    xavier,
-    Bradley
-    // Add yourself here!
-]; 
+export const teamMembers: TeamMember[] = [sharib];
--- src/team/sharib.ts
+++ src/team/sharib.ts
@@ -1,14 +1,14 @@
 import { TeamMember } from "../types/TeamMember";
 
 export const sharib: TeamMember = {
-    name: "Sharib Kabani",
-    role: "Software Engineer",
-    funFact: "I just ate some Lazeez!",
-    githubUsername: "sharibkabani",
-    // Feel free to customize your card's styles!
-    cardStyle: {
-        background: "bg-blue-900",
-        border: "border border-white-700",
-        text: "text-white"
-    }
-}; 
+  name: "Sharib Kabani",
+  role: "Software Engineer",
+  funFact: "I play soccer!",
+  githubUsername: "sharibkabani",
+  // Feel free to customize your card's styles!
+  cardStyle: {
+    background: "bg-blue-000",
+    border: "border border-slate-300",
+    text: "text-slate-300",
+  },
+};
