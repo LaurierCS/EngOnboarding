5c57d8b382a10908c9727bed1fd3ddcfd498436b
ctlchan <christakleungchan@gmail.com>
2025-09-25 23:24:36 -0400

    Add Chris to team

---
+++ src/team/chris.ts
@@ -0,0 +1,14 @@
+import { TeamMember } from "../types/TeamMember";
+
+export const chris: TeamMember = {
+  name: "Chris Chan",
+  role: "Software Engineer",
+  funFact: "I like to play volleyball and watch the Raptors.",
+  githubUsername: "ctlchan",
+  // Feel free to customize your card's styles!
+  cardStyle: {
+    background: "bg-blue-000",
+    border: "border border-slate-300",
+    text: "text-slate-300",
+  },
+};
