67c1359b9c811cd824e4bea1c14e68ac41c447d2
ctlchan <christakleungchan@gmail.com>
2025-09-25 23:24:36 -0400

    Add Chris to team

--- src/team/index.ts
+++ src/team/index.ts
@@ -2,5 +2,6 @@ import { TeamMember } from "../types/TeamMember";
 import { sharib } from "./sharib";
 import { ahmad } from "./ahmad";
 import { hunter } from "./hunter";
+import { chris } from "./chris";
 
-export const teamMembers: TeamMember[] = [sharib, ahmad, hunter];
+export const teamMembers: TeamMember[] = [sharib, ahmad, hunter, chris];
