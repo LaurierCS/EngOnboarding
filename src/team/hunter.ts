e015ec27d99a730ce3dc879d13321701f9ae8411
Hunter Shierman <hshierman@gmail.com>
2025-09-28 18:47:29 -0400

    Add hunter_shierman to team

---
+++ src/team/hunter.ts
@@ -0,0 +1,14 @@
+import { TeamMember } from "../types/TeamMember";
+
+export const hunter: TeamMember = {
+  name: "Hunter Shierman",
+  role: "Software Engineer",
+  funFact: "My favourite game is Sekiro",
+  githubUsername: "hunterShierman",
+  // Feel free to customize your card's styles!
+  cardStyle: {
+    background: "bg-gradient-to-r from-purple-600 to-blue-600",
+    border: "border border-slate-300",
+    text: "text-white font-semibold tracking-wide",
+  },
+};
