ff03e856e068269f68bb0a749d254101f4a02144
Flapjacck <plkelly333@me.com>
2025-09-29 19:06:09 -0400

    added my card

--- src/team/index.ts
+++ src/team/index.ts
@@ -3,5 +3,6 @@ import { sharib } from "./sharib";
 import { ahmad } from "./ahmad";
 import { hunter } from "./hunter";
 import { chris } from "./chris";
+import { spencer } from "./spencer";
 
-export const teamMembers: TeamMember[] = [sharib, ahmad, hunter, chris];
+export const teamMembers: TeamMember[] = [sharib, ahmad, hunter, chris, spencer];
