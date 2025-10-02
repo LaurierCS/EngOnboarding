fcb5d75db3dd781ad975e2eeba409960f676c4db
BenOkojie <83321573+BenOkojie@users.noreply.github.com>
2025-10-02 13:17:09 -0400

    added uppdated card

--- src/team/index.ts
+++ src/team/index.ts
@@ -4,5 +4,6 @@ import { ahmad } from "./ahmad";
 import { hunter } from "./hunter";
 import { chris } from "./chris";
 import { spencer } from "./spencer";
+import { benjamin } from "./benjamin";
 
-export const teamMembers: TeamMember[] = [sharib, ahmad, hunter, chris, spencer];
+export const teamMembers: TeamMember[] = [sharib, ahmad, hunter, chris, spencer, benjamin];
