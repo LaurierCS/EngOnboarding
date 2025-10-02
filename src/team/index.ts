aad2c19de44e6660b376a8578def9d79f821d43b
Carter Chan <158599225+carterchan9@users.noreply.github.com>
2025-10-02 19:40:55 -0400

    Add raafay to the teamMembers list

--- src/team/index.ts
+++ src/team/index.ts
@@ -7,5 +7,4 @@ import { spencer } from "./spencer";
 import { benjamin } from "./benjamin";
 import { raafay } from "./raafay";
 
-export const teamMembers: TeamMember[] = [sharib, ahmad, hunter, chris, spencer, benjamin];
-export const teamMembers: TeamMember[] = [sharib, ahmad, hunter, chris, spencer, raafay];
+export const teamMembers: TeamMember[] = [sharib, ahmad, hunter, chris, spencer, benjamin, raafay];
